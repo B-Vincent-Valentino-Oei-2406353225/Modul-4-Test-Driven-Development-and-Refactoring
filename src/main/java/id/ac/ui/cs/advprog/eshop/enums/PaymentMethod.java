509bd443f7fa078020f0a6e48e3245418a672518
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 01:24:17 +0700

    [REFACTOR] Add PaymentMethod enum

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
@@ -0,0 +1,30 @@
+package id.ac.ui.cs.advprog.eshop.enums;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentMethod {
+    INITIAL("INITIAL", List.of("cardNumber"));
+
+    private final String value;
+    private final List<String> requiredPaymentData;
+    PaymentMethod(String value, List<String> requiredPaymentData) {
+        this.value = value;
+        this.requiredPaymentData = requiredPaymentData;
+    }
+
+    public static boolean contains(String param) {
+        for (PaymentMethod paymentMethod : PaymentMethod.values()) {
+            if (paymentMethod.name().equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<String> getRequiredPaymentData() {
+        return this.requiredPaymentData;
+    }
+}
