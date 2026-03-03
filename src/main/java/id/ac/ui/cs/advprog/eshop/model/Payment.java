b53df178028265e6a5454d1723ff1c0c3eee85a2
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 00:47:27 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -16,8 +16,42 @@ public class Payment {
     Map<String, String> paymentData;
     
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+
+        String[] validMethod = {"INITIAL"};
+        if (Arrays.asList(validMethod).stream().noneMatch(e->(e.equals(method)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.method = method;
+        }
+
+        String[] validStatus = {"PENDING", "REJECTED", "SUCCESS"};
+        if (Arrays.asList(validStatus).stream().noneMatch(e->(e.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+        
+        Map<String, List<String>> validPaymentData = Map.of(
+            "INITIAL", List.of("cardNumber")
+        );
+        if (paymentData == null || 
+            paymentData.isEmpty() || 
+            validPaymentData.get(method).stream().anyMatch(e->(!paymentData.containsKey(e)))
+        ) {
+            throw new IllegalArgumentException();
+        } else {
+            this.paymentData = paymentData;
+        }
     }
 
     public void setStatus(String status) {
+        String[] validStatus = {"PENDING", "REJECTED", "SUCCESS"};
+        if (Arrays.asList(validStatus).stream().noneMatch(e->(e.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
