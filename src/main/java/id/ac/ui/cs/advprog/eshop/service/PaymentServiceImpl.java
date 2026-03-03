21d6c0c6efbc5eb3a00c6e98095bd9ca0f28f0c6
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 01:44:23 +0700

    [RED] Add PaymentService and PaymentService Impl skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import java.util.List;
+import java.util.Map;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+public class PaymentServiceImpl implements PaymentService {
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) { return null; }
+    public Payment setStatus(Payment payment, String status) { return null; }
+    public Payment getPayment(String paymentId) { return null; }
+    public List<Payment> getAllPayments() { return null; }
+}
