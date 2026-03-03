2e22d9f4c1d17994a66ecfcd99bddd2c585995bb
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 00:47:07 +0700

    [RED] Add Payment model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class Payment {
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
+    
+    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+    }
+
+    public void setStatus(String status) {
+    }
+}
