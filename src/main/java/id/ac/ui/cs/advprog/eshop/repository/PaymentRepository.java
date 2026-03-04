b79a58ab89e1422f5ec8d697845d5901e90ad2a1
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 10:12:16 +0700

    [REFACTOR] Add @Repository Annotation for PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -3,8 +3,11 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 
+@Repository
 public class PaymentRepository {
     private final List<Payment> paymentData = new ArrayList<>();
 
