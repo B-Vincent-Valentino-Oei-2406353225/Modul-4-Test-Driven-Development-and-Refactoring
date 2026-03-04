5ba65abfdc194b879c9ce0757cf283736e1aee73
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 10:11:35 +0700

    [REFACTOR] Annotate all payment strategy with @Component

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/payment/CashOnDeliveryStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/payment/CashOnDeliveryStrategy.java
@@ -2,8 +2,11 @@ package id.ac.ui.cs.advprog.eshop.service.payment;
 
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 
+@Component
 public class CashOnDeliveryStrategy implements PaymentStrategy {
     @Override
     public boolean supports(String method) {
@@ -14,7 +17,7 @@ public class CashOnDeliveryStrategy implements PaymentStrategy {
     public String processPayment(Map<String, String> paymentData) {
         String address = paymentData.get("address");
         String deliveryFee = paymentData.get("deliveryFee");
-        
+
         if (address == null || address.isEmpty() || deliveryFee == null || deliveryFee.isEmpty()) {
             return PaymentStatus.REJECTED.getValue();
         } else {
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/payment/VoucherStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/payment/VoucherStrategy.java
@@ -2,8 +2,11 @@ package id.ac.ui.cs.advprog.eshop.service.payment;
 
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
+
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 
+@Component
 public class VoucherStrategy implements PaymentStrategy {
     @Override
     public boolean supports(String method) {
