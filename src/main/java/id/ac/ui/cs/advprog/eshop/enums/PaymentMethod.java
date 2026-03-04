095f1f2eb4632fd95dc3904e668f649369b86465
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 09:47:59 +0700

    [RED] Add CASH_ON_DELIVERY Payment Method

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum PaymentMethod {
     INITIAL("INITIAL"),
-    VOUCHER("VOUCHER");
+    VOUCHER("VOUCHER"),
+    CASH_ON_DELIVERY("CASH_ON_DELIVERY");
 
     private final String value;
     PaymentMethod(String value) {
