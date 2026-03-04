e1ef66bbebc5d57d4cbb424a78acb9273e7fb890
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 10:13:19 +0700

    [REFACTOR] Apply PaymentStrategy to PaymentServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -3,52 +3,41 @@ package id.ac.ui.cs.advprog.eshop.service;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
-import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import id.ac.ui.cs.advprog.eshop.service.payment.PaymentStrategy;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
-    @Autowired
     private PaymentRepository paymentRepository;
+    private List<PaymentStrategy> paymentStrategies;
+
+    public PaymentServiceImpl(PaymentRepository paymentRepository, List<PaymentStrategy> paymentStrategies) {
+        this.paymentRepository = paymentRepository;
+        this.paymentStrategies = paymentStrategies;
+    }
 
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
         if (order == null || method == null || paymentData == null) {
             throw new IllegalArgumentException();
         }
+
         if (paymentRepository.findById(order.getId()) != null) {
             return null;
         }
 
-        String paymentStatus = PaymentStatus.PENDING.getValue();
-        if (PaymentMethod.VOUCHER.getValue().equals(method) && !isValidVoucherData(paymentData)) {
-            paymentStatus = PaymentStatus.REJECTED.getValue();
-        } else if (PaymentMethod.CASH_ON_DELIVERY.getValue().equals(method) && !isValidCashOnDeliveryData(paymentData)) {
-            paymentStatus = PaymentStatus.REJECTED.getValue();
-        }
+        String paymentStatus = paymentStrategies.stream()
+            .filter(strategy -> strategy.supports(method))
+            .findFirst()
+            .orElseThrow(IllegalArgumentException::new)
+            .processPayment(paymentData);
 
         Payment payment = new Payment(order.getId(), method, paymentStatus, paymentData);
         paymentRepository.save(payment);
         return payment;
-    }
-
-    private boolean isValidVoucherData(Map<String, String> paymentData) {
-        String voucherCode = paymentData.get("voucherCode");
-        if (voucherCode == null) {
-            return false;
-        }
-
-        String regex = "^ESHOP(?=(?:\\D*\\d){8}\\D*$).{11}$";
-        return voucherCode.matches(regex);
-    }
-
-    private boolean isValidCashOnDeliveryData(Map<String, String> paymentData) {
-        String address = paymentData.get("address");
-        String deliveryFee = paymentData.get("deliveryFee");
-        return address != null && !address.isEmpty() && deliveryFee != null && !deliveryFee.isEmpty();
     }
 
     public Payment setStatus(Payment payment, String status) {
