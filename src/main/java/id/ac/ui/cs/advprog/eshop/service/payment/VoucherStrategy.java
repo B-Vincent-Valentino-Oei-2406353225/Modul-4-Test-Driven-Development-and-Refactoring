d684af203f40cc9bbb57febf1857e8789759012b
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 09:54:57 +0700

    [REFACTOR] Add VoucherStrategy'

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/payment/VoucherStrategy.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.eshop.service.payment;
+
+import java.util.Map;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+
+public class VoucherStrategy implements PaymentStrategy {
+    @Override
+    public boolean supports(String method) {
+        return "VOUCHER".equals(method);
+    }
+
+    @Override
+    public String processPayment(Map<String, String> paymentData) {
+        String voucherCode = paymentData.get("voucherCode");
+        String regex = "^ESHOP(?=(?:\\D*\\d){8}\\D*$).{11}$";
+
+        if (voucherCode == null || !voucherCode.matches(regex)) {
+            return PaymentStatus.REJECTED.getValue();
+        } else {
+            return PaymentStatus.PENDING.getValue();
+        }
+    }
+}
