61ae03f42a252091935ae3b976a6f97d3c3f6ff0
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2026-03-04 09:51:41 +0700

    [REFACTOR] Add DummyStrategy for INITIAL payment method

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/payment/DummyStrategy.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eshop.service.payment;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+
+public class DummyStrategy implements PaymentStrategy {
+    @Override
+    public boolean supports(String method) {
+        return "INITIAL".equals(method);
+    }
+
+    @Override
+    public String processPayment(String paymentData) {
+        return PaymentStatus.PENDING.getValue();
+    }
+}
